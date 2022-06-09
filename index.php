74481a1aa26ab10e35099d7cd5a54553fa89488c
finmancon <77546178+finmancon@users.noreply.github.com>
2022-06-09 13:55:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <body>
-<h1>Hello World</h1>
+<h1>Hello World !</h1>
 <p>I'm hosted with GitHub Pages.</p>
-<?php echo date("Y");?>
+<?php echo "Year is: ".date("Y");?>
 </body>
 </html>
